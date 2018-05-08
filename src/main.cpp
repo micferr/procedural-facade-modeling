90a24b26553162f117f02de590457aefa1e8a068
Michele Ferraro <micfer96@gmail.com>
2018-05-09 00:31:16 +0200

    Working Bezier example

--- src/main.cpp
+++ src/main.cpp
@@ -14,21 +14,6 @@
 #include "prob_utils.h"
 #include "yocto_utils.h"
 
-long fact(long n) {
-	long res = 1;
-	for (int i = 2; i <= n; i++) {
-		res *= i;
-	}
-	return res;
-}
-
-long binomial(long n, long k) {
-	if (n >= k && k >= 0) {
-		return fact(n) / (fact(k) * fact(n - k));
-	}
-	else return 0;
-}
-
 template<class Point>
 class bezier {
 public:
@@ -36,24 +21,50 @@ public:
 
 	Point compute(float t) {
 		auto n = control_points.size();
-		Point p;
-		// Raw Bezier formula - This is bad! :'(
-		for (int i = 0; i <= n; i++) {
-			p += (control_points[i] * binomial(n, i) * powf(1.f - t, (float)n - i) * powf(t, (float)i));
+		if (n == 0) return { 0,0 };
+
+		// De Casteljau
+		auto points = control_points;
+		std::vector<Point> points2;
+		while (points.size() > 1) {
+			for (int i = 0; i < points.size() - 1; i++) {
+				points2.push_back({ points[i] + (points[i + 1] - points[i])*t });
+			}
+			points = points2;
+			points2.clear();
 		}
-		return p;
+		return points[0];
 	}
 };
 
 int main(int argc, char** argv) {
 	bezier<ygl::vec2f> my_bezier;
-	for (auto i = 0; i <= 6; i++) {
-		auto j = float(i)*ygl::pi/6;
-		my_bezier.control_points.push_back({ j, sin(j) });
+	my_bezier.control_points.push_back({ 0,0 });
+	my_bezier.control_points.push_back({ 2,4 });
+	my_bezier.control_points.push_back({ 4,-4 });
+	my_bezier.control_points.push_back({ 6,0 });
+	for (int i = 0; i < 20; i++) {
+		float perc = float(i) / 20.f;
+		printf("%i\t%f\t%f\n", i + 1, my_bezier.compute(perc).x, my_bezier.compute(perc).y);
 	}
-	for (auto i = 0; i <= 30; i++) {
-		auto j = float(i)*ygl::pi / 30;
-		printf("%d\t| %f\t%f\t%f\t%f\n", i, j, sin(j), my_bezier.compute(i/30.f).x, my_bezier.compute(i/30.f).y);
+
+	ygl::shape sphere_shp;
+	ygl::make_cube(sphere_shp.quads, sphere_shp.pos, 5, .1f);
+	ygl::material* sphere_mat = ygl::make_material("sphere_mat");
+	std::vector<ygl::instance*> sphere_insts;
+	for (int i = 0; i < 20; i++) {
+		ygl::frame3f sphere_frame;
+		sphere_frame.o = yb::to_3d(my_bezier.compute(float(i) / 20.f));
+		sphere_insts.push_back(ygl::make_instance(
+			"sphere_inst" + std::to_string(i),
+			&sphere_shp,
+			sphere_mat,
+			sphere_frame
+		));
 	}
+	ygl::scene scene;
+	for (auto inst : sphere_insts) yb::add_to_scene(&scene, inst);
+	ygl::save_scene("ciao.obj", &scene);
+
 	return 0;
 }
